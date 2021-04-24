2e77e54f976a9a260d3fefbb8df6e70e8f799d1c
jh20s <jh20s@naver.com>
2021-04-24 17:15:35 +0900

    FlyWeight Pattern

---
+++ Flyweight Pattern/Assets/Terrain.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain
+{
+    public bool isWater = false;
+    public int moveCost = 0;
+    public string objectName = "";
+
+    public Terrain(bool _isWater, int _moveCost, string _objectName)
+    {
+        isWater = _isWater;
+        moveCost = moveCost;
+        objectName = _objectName;
+    }
+}
