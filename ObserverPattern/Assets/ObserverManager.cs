861144e90cf6e6b5440de7c251716b82b56d18a0
jh20s <jh20s@naver.com>
2021-04-24 19:36:34 +0900

    ObserverPattern

---
+++ ObserverPattern/Assets/ObserverManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObserverManager : MonoBehaviour
+{
+    static public ObserverManager Instance;
+
+    public delegate void ClickButton();
+    private event ClickButton ClickEvent;
+    public void ObservClickEvent(ClickButton obs)
+    {
+        ClickEvent += obs;
+    }
+
+    public void ClickMyButton()
+    {
+        ClickEvent.Invoke();
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+}
---
+++ ObserverPattern/Assets/ObservingButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObservingButton : MonoBehaviour
+{
+    bool check = true;
+    GameObject obj;
+    public void SetGameObject()
+    {
+        check = !check;
+        obj.SetActive(check);
+    }
+    void Start()
+    {
+        obj = GameObject.Find("Cube");
+        ObserverManager.Instance.ObservClickEvent(SetGameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
